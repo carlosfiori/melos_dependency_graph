ac284cb39f57727ba6ffa52faf8d69d839c22596
Carlos Henrique Fiori Petrechen <carlos.fiori@gmail.com>
2025-06-07 11:17:41 -0300

    Initial

---
+++ lib/melos_dependency_graph.dart
@@ -0,0 +1,10 @@
+/// melos_dependency_graph, A Very Good Project created by Very Good CLI.
+///
+/// ```sh
+/// # activate melos_dependency_graph
+/// dart pub global activate melos_dependency_graph
+///
+/// # see usage
+/// melos_dependency_graph --help
+/// ```
+library;
